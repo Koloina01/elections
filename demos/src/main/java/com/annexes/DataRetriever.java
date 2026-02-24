c0a56b60239bd4529b872c36821035e2fb78ae80
Koloina01 <kolomaherison@gmail.com>
2026-02-24 21:30:50 +0300

    feat: implement method to count votes by type

--- demos/src/main/java/com/annexes/DataRetriever.java
+++ demos/src/main/java/com/annexes/DataRetriever.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DataRetriever {
 
@@ -12,8 +16,8 @@ public class DataRetriever {
         String sql = "SELECT COUNT(id) AS total_votes FROM vote";
 
         try (Connection connection = DBConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()) {
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet resultSet = statement.executeQuery()) {
 
             if (resultSet.next()) {
                 return resultSet.getLong("total_votes");
@@ -24,5 +28,38 @@ public class DataRetriever {
         } catch (SQLException e) {
             throw new RuntimeException("Error while counting votes", e);
         }
+    }
+
+    public List<VoteTypeCount> countVotesByType() {
+
+        List<VoteTypeCount> results = new ArrayList<>();
+
+        String sql = """
+                SELECT vote_type, COUNT(id) AS nombre_votes
+                FROM vote
+                GROUP BY vote_type
+                ORDER BY CASE vote_type
+                    WHEN 'VALID' THEN 1
+                    WHEN 'BLANK' THEN 2
+                    WHEN 'NULL' THEN 3
+                END
+                """;
+
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery()) {
+
+            while (rs.next()) {
+                VoteType voteType = VoteType.valueOf(rs.getString("vote_type"));
+                int count = rs.getInt("nombre_votes");
+
+                results.add(new VoteTypeCount(voteType, count));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error counting votes by type", e);
+        }
+
+        return results;
     }
 }
