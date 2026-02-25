537c34a8602466e96f1e5d7a19364161b0671ca7
Koloina01 <kolomaherison@gmail.com>
2026-02-25 09:56:59 +0300

    feat: add ElectionResult.java

---
+++ demos/src/main/java/com/annexes/ElectionResult.java
@@ -0,0 +1,25 @@
+package com.annexes;
+
+public class ElectionResult {
+
+    private String candidateName;
+    private int validVoteCount;
+    public ElectionResult(String candidateName, int validVoteCount) {
+        this.candidateName = candidateName;
+        this.validVoteCount = validVoteCount;
+    }
+
+    public String getCandidateName() {
+        return candidateName;
+    }
+
+    public int getValidVoteCount() {
+        return validVoteCount;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectionResult(candidateName=" + candidateName +
+               ", validVoteCount=" + validVoteCount + ")";
+    }
+}
